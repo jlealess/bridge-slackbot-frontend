2b63c303aab948b7efacc8719dd647269c0493cc
Jackie Lealess <jlealess@gmail.com>
2018-11-07 19:48:08 -0500

    Working on success message component

--- src/components/SuccessMessage.js
+++ src/components/SuccessMessage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+<<<<<<< HEAD
 import styled from "styled-components";
 
 const StyledMessage = styled.div`
@@ -13,6 +14,14 @@ const SuccessMessage = ({ savedPollId }) => (
     Poll created!{" "}
     <Link to={`/poll/${savedPollId}`}>View poll</Link>
   </StyledMessage>
+=======
+
+const SuccessMessage = ({ savedPollId }) => (
+  <div>
+    Poll created!{" "}
+        <Link to={/polls/`${savedPollId}`}>View poll</Link>
+  </div>
+>>>>>>> Working on success message component
 );
 
 export default SuccessMessage;
