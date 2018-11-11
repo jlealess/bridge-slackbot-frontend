94f550c9fad78b1b05104a27402a11fd35a56c38
Jackie Lealess <jlealess@gmail.com>
2018-11-11 09:40:04 -0500

    Fixing merge conflict from upstream

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,8 @@
 import React from "react";
-<<<<<<< HEAD
 import { Switch, Route, Redirect } from "react-router-dom";
 import NewPoll from "./NewPoll/NewPoll";
 import Poll from "./Poll/Poll";
-<<<<<<< HEAD
 import Polls from "./Polls/Polls";
-=======
->>>>>>> Adds new poll component, success message on form submit, and prevents empty form submission
-=======
-import { Switch, Route, Link, } from "react-router-dom";
-import NewPoll from "./NewPoll/NewPoll";
-import Poll from "./Poll/Poll";
-import Polls from "./Polls/Polls";
->>>>>>> Adding component that shows all polls
 
 const Main = () => (
   <div className="main" role="main">
