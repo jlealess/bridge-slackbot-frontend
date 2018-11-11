4fdb73537a1735d90f80bac9cdac80cc42150c41
Jackie Lealess <jlealess@gmail.com>
2018-11-11 13:24:25 -0500

    Adding nav links to header

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import NewPoll from "./NewPoll/NewPoll";
 import Poll from "./Poll/Poll";
 import Polls from "./Polls/Polls";
@@ -8,7 +8,7 @@ const Main = () => (
   <div className="main" role="main">
     <div className="content-wrapper">
       <Switch>
-        <Route exact path="/" component={NewPoll} />
+        <Route exact path="/" render={() => <Redirect to="/new-poll" />} />
         <Route path="/new-poll" component={NewPoll} />
         <Route path="/polls" component={Polls} />
         <Route path="/poll/:pollId" component={Poll} />>
